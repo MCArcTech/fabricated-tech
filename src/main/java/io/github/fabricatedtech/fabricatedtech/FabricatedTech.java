3bca1b69b1adf2879a280e529aa39f94449c4796
MrMelon <github@onpointcoding.net>
2022-03-13 17:37:18 +0000

    First commit 3

---
+++ src/main/java/io/github/fabricatedtech/fabricatedtech/FabricatedTech.java
@@ -0,0 +1,10 @@
+package io.github.fabricatedtech.fabricatedtech;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricatedTech implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
